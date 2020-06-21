3f6429e5eb66f43274a0000794701bc2ff04ac7d
SabrinaM98 <sabrinarolim98@gmail.com>
2020-06-20 21:45:55 -0300

    ADD: classCangaceiro

---
+++ cangaceiro.py
@@ -0,0 +1,18 @@
+class Cangaceiro:
+    def __init__(self, devocao: int, temor: int, insanidade: int, rapadura: int):
+        self.mandonismo = mandonismo
+        self.diplomacia = diplomacia
+        self.municao = municao
+        self.poder_aquisitivo = poder_aquisitivo
+
+    def se_aquiete_homi(self, devocao: int, ataque_inimigo: int):
+        pass
+
+    def cristo_jesus_me_tira_dessa(self, rapadura: int, pontos_vida: int):
+        pass
+
+    def quero_briga_de_foice(self, pontos_mana: int, insanidade: int):
+        pass
+
+    def gaita_curandeira(self, devocao: int, ataque_inimigo: int):
+        pass
