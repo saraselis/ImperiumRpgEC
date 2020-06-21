302fd9ecced8aec948cf104a5d01fd3e75c5f369
SabrinaM98 <sabrinarolim98@gmail.com>
2020-06-20 21:44:24 -0300

    ADD: classCoronel

---
+++ coronel.py
@@ -0,0 +1,21 @@
+class Coronel:
+    def __init__(self, mandonismo: int, diplomacia: int, municao: int, poder_aquisitivo: int):
+        self.mandonismo = mandonismo
+        self.diplomacia = diplomacia
+        self.municao = municao
+        self.poder_aquisitivo = poder_aquisitivo
+
+    def toma_bala(self, municao: int, pontos_ataque: int, poder_aquisitivo: int):
+        pass
+
+    def hj_nao_cabra(self, diplomacia: int, municao: int, ataque_inimigo: int):
+        pass
+
+    def passa_merthiolate(self, poder_aquisitivo: int, pontos_vida: int):
+        pass
+
+    def maculele(self, pontos_mana: int, mandonismo: int, municao: int):
+        pass
+
+    def pipoco_dos_tiro(self, municao: int, mandonismo: int, pontos_ataque: int):
+        pass
